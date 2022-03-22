b8e53d8b368990a87ae0d12c9a868aa5aed549e3
MarziehMoradian <marziehmoradiann@gmail.com>
2022-03-23 01:58:38 +0430

    add toolkit

---
+++ src/features/customerSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CustomerState {
+  value: Customer[];
+}
+
+interface AddFoodCustomerPayload {
+  food: string;
+  id: string;
+}
+
+interface Customer {
+  id: string;
+  name: string;
+  food: string[];
+}
+
+const initialState: CustomerState = {
+  value: [],
+};
+
+export const customerSlice = createSlice({
+  name: "customers",
+  initialState,
+  reducers: {
+    addCustomer: (state, action: PayloadAction<Customer>) => {
+      state.value.push(action.payload);
+    },
+    addFoodCustomer: (state, action: PayloadAction<AddFoodCustomerPayload>) => {
+      state.value.forEach((customer) => {
+        if (customer.id === action.payload.id) {
+          customer.food.push(action.payload.food);
+        }
+      });
+    },
+  },
+});
+
+export const { addCustomer, addFoodCustomer } = customerSlice.actions;
+
+export default customerSlice.reducer;
